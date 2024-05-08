0a997329a2b1ea1e35b3df6deb21414642d4dd21
brynova <bryant321e@gmail.com>
2024-05-08 11:58:24 -0400

    Refrencing the MDN createElement documention

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,5 +1,13 @@
+//Refrencing the MDN "createElement() documentation for implemenation"
+document.window.onload = addElement;
+    function addElement() {
+        const newMessage = document.createElement("input");
+        const newContent = document.createTextNode("Enter a matrix size: ");
+        const newType = document.createElement("Type=")
+        const input = document.input.newType("integers");
+    }
+
 window.onload = () => {
-    // Add your code here
     window.alert('Enter a matrix size: ' );
     alert('Enter a matrix size: ' );
 };
